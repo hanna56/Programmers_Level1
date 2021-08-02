9080ca6d135043510d9fdfe6d1e054d4943455ac
Hanna <58249219+hanna56@users.noreply.github.com>
2021-08-02 17:04:41 +0900

    Create 음양 더하기.py

---
+++ 음양 더하기.py
@@ -0,0 +1,15 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == 1:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+            
+    return answer
+  
+  
+  
+# 다른 풀이
+def solution(absolutes, signs):
+    return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
