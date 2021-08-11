c73cda329696018ff98e6943cf1c6458a46c73d1
Hanna <58249219+hanna56@users.noreply.github.com>
2021-08-11 21:14:33 +0900

    Update 정수 제곱근 판별.py

--- 정수 제곱근 판별.py
+++ 정수 제곱근 판별.py
@@ -1,6 +1,3 @@
 import numpy as np
 def solution(n):
-    if int(np.sqrt(n)) == np.sqrt(n):
-        return (np.sqrt(n)+1)**2
-    else:
-        return -1
+    return (np.sqrt(n)+1)**2 if int(np.sqrt(n))==np.sqrt(n) else -1
