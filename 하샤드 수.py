8af078732633cba18ecc0838f23b163f889c9f85
Hanna <58249219+hanna56@users.noreply.github.com>
2021-08-14 16:34:34 +0900

    Update 하샤드 수.py

--- 하샤드 수.py
+++ 하샤드 수.py
@@ -1,12 +1,10 @@
 def solution(x):
     sum_x = 0
     for i in str(x):
-        sum_x+=int(i)
-        
+        sum_x+=int(i)        
     return x % sum_x == 0
 
   
 # 다른풀이
 def solution(x):
-    return x % sum([int(num) for num in list(str(x))]) == 0
-        
+    return x % sum([int(num) for num in str(x)]) == 0
